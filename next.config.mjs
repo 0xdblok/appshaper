8db0461a4bfe8863894bc2dc87cd06fecea7f404
0xdblok <dblok0x@gmail.com>
2025-09-27 21:23:19 +0200

    ee

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: { ignoreDuringBuilds: true },
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'placehold.co',
-          pathname: '/**',
-        },
-      ],
+      remotePatterns: [{ protocol: 'https', hostname: 'placehold.co', pathname: '/**' }],
     },
   };
-  
   export default nextConfig;
